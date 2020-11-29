8ed74a0d1492fef8c010c7abd2721fc297d31724
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-11-29 21:16:22 +0530

    added static root

--- hospital/settings.py
+++ hospital/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
