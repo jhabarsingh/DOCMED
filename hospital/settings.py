79a3704ededc8772556ad17c2b37a61ca17afcf9
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-11-29 18:22:08 +0530

    added collectstatic files

--- hospital/settings.py
+++ hospital/settings.py
@@ -127,9 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
