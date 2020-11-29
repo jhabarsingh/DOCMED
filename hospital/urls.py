faef865b2a9090d581d931ceafd19be1a26908e5
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-11-29 18:30:35 +0530

    fixed heroku bugs

--- hospital/urls.py
+++ hospital/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
