f25e2f2e4093a0c36f39529de43a30f29d75c9be
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-11-29 18:15:21 +0530

    home page completed heroku setting files added

--- hospital/settings.py
+++ hospital/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'putdgisv',
+        'USER': 'putdgisv',
+        'PASSWORD': 'V_SrqNuZUfFuffVJrYxVcC1qCDlztTH6',
+        'HOST': 'suleiman.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
