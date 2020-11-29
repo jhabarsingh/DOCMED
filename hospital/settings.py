816d95e2ccee4b58011a275eae29475ea8b3915b
jhabarsingh <jhabarsinghbhati23@gmail.com>
2020-11-29 21:52:46 +0530

    upgraded whitenoise

--- hospital/settings.py
+++ hospital/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
---
+++ staticfiles/js/main.983d1edfe0ed.js
@@ -0,0 +1,12 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.onload = () => {
+	
+}
