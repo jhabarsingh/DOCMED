c5a6dd35711135a5e98604837ba5049e8e915ee2
jhabar <jhabarsinghbhati23@gmail.com>
2021-01-08 02:21:58 +0530

    renamed project to docmed

--- docmed/wsgi.py
+++ docmed/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docmed.settings')
 
 application = get_wsgi_application()
