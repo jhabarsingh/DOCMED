d6d6b3e57ebf0e4d5896803f6182ca72c8674e2f
jhabar <jhabarsinghbhati23@gmail.com>
2021-02-22 22:20:33 +0530

    Removed the xray classifier model

--- xray_classifier/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from torch_engine import *
-from torch_model import *
-from torch_data import *
-from torch_train import *
-from torch_infer import *
-from utils import *
--- xray_classifier/model/serve_waitress.py
+++
@@ -1,6 +0,0 @@
-# Serve the flask app with wsgi using waitress
-
-from waitress import serve
-import web_app
-
-serve(web_app.app, port=9001, threads=6)
