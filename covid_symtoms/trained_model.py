e4f8b24258a76d4ba96430bf787006a20ea658f3
Jhabar Singh Bhati <43932986+jhabarsingh@users.noreply.github.com>
2021-02-23 14:01:14 +0530

    Updated Trained Model

--- covid_symtoms/trained_model.py
+++ covid_symtoms/trained_model.py
@@ -14,22 +14,18 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-
-
 from sklearn.ensemble import RandomForestClassifier
 rfc1=RandomForestClassifier(criterion= 'gini', max_depth= 4, max_features= 'sqrt', n_estimators= 100)
-
-
-
-
 binary_file = open('data.obj', 'rb')
 rfc1 = pickle.load(binary_file)
 binary_file.close()
+
 import random
+
 a = [0, 1, 1, 1, 0]
 for i in range(5, 23):
     a.append(1)
-pred=rfc1.predict([a])
-print(pred)
+pred = rfc1.predict([a])
+# print(pred)
 
 
