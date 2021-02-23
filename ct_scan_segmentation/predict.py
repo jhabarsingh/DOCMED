68c4fc8c244cc6dee5abe3875ab16ff479d5a072
jhabar <jhabarsinghbhati23@gmail.com>
2021-02-23 09:54:00 +0530

    added ct scan segmentation

---
+++ ct_scan_segmentation/predict.py
@@ -0,0 +1,212 @@
+# %% [code]
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import nibabel as nib
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+
+# %% [code]
+data = pd.read_csv('../input/covid19-ct-scans/metadata.csv')
+data.head()
+
+# %% [code]
+def read_nii(filepath):
+    '''
+    Reads .nii file and returns pixel array
+    '''
+    ct_scan = nib.load(filepath)
+    array   = ct_scan.get_fdata()
+    array   = np.rot90(np.array(array))
+    return(array)
+
+# %% [code]
+# Read sample
+sample_ct   = read_nii(data.loc[1,'ct_scan'])
+sample_lung = read_nii(data.loc[1,'lung_mask'])
+sample_infe = read_nii(data.loc[1,'infection_mask'])
+sample_all  = read_nii(data.loc[1,'lung_and_infection_mask'])
+
+# %% [code]
+fig = plt.figure(figsize = (18,15))
+plt.subplot(1,4,1)
+plt.imshow(sample_ct[..., 150], cmap = 'bone')
+plt.title('Original Image')
+
+plt.subplot(1,4,2)
+plt.imshow(sample_ct[..., 150], cmap = 'bone')
+plt.imshow(sample_lung[..., 150],alpha = 0.5, cmap = 'nipy_spectral')
+plt.title('Lung Mask')
+
+plt.subplot(1,4,3)
+plt.imshow(sample_ct[..., 150], cmap = 'bone')
+plt.imshow(sample_infe[..., 150], alpha = 0.5, cmap = 'nipy_spectral')
+plt.title('Infection Mask')
+
+plt.subplot(1,4,4)
+plt.imshow(sample_ct[..., 150], cmap = 'bone')
+plt.imshow(sample_all[..., 150], alpha = 0.5, cmap = 'nipy_spectral')
+plt.title('Lung and Infection Mask')
+
+# %% [markdown]
+# ## Load Data
+
+# %% [code]
+lungs = []
+infections = []
+img_size = 128
+
+for i in range(len(data)):
+    ct = read_nii(data['ct_scan'][i])
+    infect = read_nii(data['infection_mask'][i])
+    
+    for ii in range(ct.shape[0]):
+        lung_img = cv2.resize(ct[ii], dsize = (img_size, img_size),interpolation = cv2.INTER_AREA).astype('uint8')
+        infec_img = cv2.resize(infect[ii],dsize=(img_size, img_size),interpolation = cv2.INTER_AREA).astype('uint8')
+        lungs.append(lung_img[..., np.newaxis])
+        infections.append(infec_img[..., np.newaxis])
+
+# %% [code]
+lungs = np.array(lungs)
+infections = np.array(infections)
+
+# %% [code]
+print(lungs.shape)
+
+# %% [code]
+print(infections.shape)
+
+# %% [code]
+from sklearn.model_selection import train_test_split
+lung_train, lung_test, infect_train, infect_test = train_test_split(lungs, infections, test_size = 0.1)
+
+# %% [code]
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+
+def build_model(input_layer, start_neurons):
+    # 128 -> 64
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(input_layer)
+    conv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(0.25)(pool1)
+
+    # 64 -> 32
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(0.5)(pool2)
+
+    # 32 -> 16
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(0.5)(pool3)
+
+    # 16 -> 8
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(pool3)
+    conv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(conv4)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(0.5)(pool4)
+
+    # Middle
+    convm = Conv2D(start_neurons * 16, (3, 3), activation="relu", padding="same")(pool4)
+    convm = Conv2D(start_neurons * 16, (3, 3), activation="relu", padding="same")(convm)
+
+    # 8 -> 16
+    deconv4 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(0.5)(uconv4)
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(uconv4)
+    uconv4 = Conv2D(start_neurons * 8, (3, 3), activation="relu", padding="same")(uconv4)
+
+    # 16 -> 32
+    deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(0.5)(uconv3)
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(uconv3)
+    uconv3 = Conv2D(start_neurons * 4, (3, 3), activation="relu", padding="same")(uconv3)
+
+    # 32 -> 64
+    deconv2 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+    uconv2 = Dropout(0.5)(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(uconv2)
+    uconv2 = Conv2D(start_neurons * 2, (3, 3), activation="relu", padding="same")(uconv2)
+
+    # 64 -> 128
+    deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+    uconv1 = Dropout(0.5)(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(uconv1)
+    uconv1 = Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(uconv1)
+
+    #uconv1 = Dropout(0.5)(uconv1)
+    output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    
+    return output_layer
+
+input_layer = Input((img_size, img_size, 1))
+output_layer = build_model(input_layer, 16)
+
+# %% [code]
+output = build_model(input_layer, 16)
+
+# %% [code]
+model = Model(input_layer, output_layer)
+
+# %% [code]
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# %% [code]
+model.summary()
+
+# %% [code]
+history = model.fit(lung_train, infect_train, epochs = 10, validation_data = (lung_test, infect_test))
+
+# %% [code]
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Accuracy vs Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Val'], loc = 'upper left')
+plt.show()
+
+# %% [code]
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss vs Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(['Train', 'Val'], loc = 'upper right')
+plt.show()
+
+# %% [code]
+len(lung_test)
+
+# %% [markdown]
+# predicted masks
+
+# %% [code]
+predicted = model.predict(lung_test)
+fig = plt.figure(figsize = (18,15))
+
+plt.subplot(1,3,1)
+plt.imshow(lung_test[550][...,0], cmap = 'bone')
+plt.title('original lung')
+
+plt.subplot(1,3,2)
+plt.imshow(lung_test[550][...,0], cmap = 'bone')
+plt.imshow(infect_test[550][...,0],alpha = 0.5, cmap = "nipy_spectral")
+plt.title('original infection mask')
+
+plt.subplot(1,3,3)
+plt.imshow(lung_test[550][...,0], cmap = 'bone')
+plt.imshow(predicted[550][...,0],alpha = 0.5,cmap = "nipy_spectral")
+plt.title('predicted infection mask')
